9c164aacd19147781e7f87da5d1d95845296d09f
QuaRang <dbduddnd1225@gmail.com>
2024-03-20 23:40:22 +0900

    #6 - 아이콘 변경
    - 아이콘 다지안 후 여러 디바이스 별로 생성
    - 테스트뷰 코드 수정

--- PokeDex_Project/Test1View.swift
+++ PokeDex_Project/Test1View.swift
@@ -28,17 +28,6 @@ class TreeNode: Identifiable {
     }
 }
 
-// 이진 트리를 생성하는 함수 정의
-//func createBinaryTree() -> TreeNode {
-//    let node7 = TreeNode(value: 7)
-//    let node6 = TreeNode(value: 6)
-//    let node5 = TreeNode(value: 5)
-//    let node4 = TreeNode(value: 4)
-//    let node3 = TreeNode(value: 3, left: node6, right: node7)
-//    let node2 = TreeNode(value: 2, left: node4, right: node5)
-//    let root = TreeNode(value: 1, left: node2, right: node3)
-//    return root
-//}
 
 // 트리의 각 노드를 표시하는 SwiftUI 뷰 정의
 struct TreeNodeView: View {
@@ -54,11 +43,15 @@ struct TreeNodeView: View {
                         .foregroundColor(.white)
                         .font(.headline)
                 )
-            HStack {
+            HStack(alignment: .top){
                 ForEach(node.children) { child in
-                    TreeNodeView(node: child)
+                    VStack{
+                        Image(systemName: "chevron.down")
+                            .padding(.bottom)
+                        TreeNodeView(node: child)
+                    }
                 }
-            }
+            }.padding()
         }
     }
 }
@@ -86,12 +79,13 @@ struct Test1View: View {
 // ContentView를 미리보기
 struct Test1View_Previews: PreviewProvider {
     static var previews: some View {
+            let node10 = TreeNode(value: 10)
             let node4 = TreeNode(value: 4)
             let node5 = TreeNode(value: 5)
             let node6 = TreeNode(value: 6)
-            let node7 = TreeNode(value: 7)
-            let node2 = TreeNode(value: 2, children: [node4, node5])
-            let node3 = TreeNode(value: 3, children: [node6, node7])
+            let node7 = TreeNode(value: 7,children: [node10])
+            let node2 = TreeNode(value: 2, children: [node4, node5,node6])
+            let node3 = TreeNode(value: 3, children: [node7])
             let treeRoot = TreeNode(value: 1, children: [node2, node3])
             return Test1View(treeRoot: treeRoot)
         }
