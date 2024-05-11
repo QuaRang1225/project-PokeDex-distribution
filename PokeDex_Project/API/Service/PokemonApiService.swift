8caab8c39a6091e77d7b65a7112e8b1c5753f346
QuaRang <dbduddnd1225@gmail.com>
2024-05-11 21:12:36 +0900

    #8 - 포켓몬 정보 요청 관련
    - 특정 ID를 가진 포켓몬,포켓몬 리스트 요청 구현
    - 라우터를 생성해 요청별로 옵션 부여
    - API Manager를 생성해 전역으로 사용할 수 있도록 모듈화

--- PokeDex_Project/API/Service/PokemonApiService.swift
+++ PokeDex_Project/API/Service/PokemonApiService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+enum PokemonApiService{
+    static func pokemon(id:Int)-> AnyPublisher<PokemonResponse,AFError>{
+        print("포켓몬 api 호출")
+        return ApiClient.shared.session
+            .request(PokemonRouter.pokemon(id: id))
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: PokemonResponse.self)
+            .value()
+            .eraseToAnyPublisher()
+    }
+    
+    static func pokemons(page:Int,region:String,type_1:String,type_2:String,query:String)-> AnyPublisher<PokemonListResponse,AFError>{
+        print("포켓몬 api 호출")
+        return ApiClient.shared.session
+            .request(PokemonRouter.pokemons(page: page, region: region, type_1: type_1, type_2: type_2, query: query))
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: PokemonListResponse.self)
+            .value()
+            .eraseToAnyPublisher()
+    }
+}
