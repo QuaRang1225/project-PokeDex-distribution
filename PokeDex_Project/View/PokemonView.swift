dff3f4d57f4a84ccb9ba102ddfd8c985ee2d8d01
QuaRang <dbduddnd1225@gmail.com>
2024-05-17 23:08:57 +0900

    #8 - 파일 모듈화
    - 몬스터볼이미지 컴포넌츠와 타입 뷰를 분리해서 모듈화

---
+++ PokeDex_Project/View/PokemonView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonView.swift
+//  PokeDex_Project
+//
+//  Created by 유영웅 on 5/17/24.
+//
+
+import SwiftUI
+
+struct PokemonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonView()
+}
