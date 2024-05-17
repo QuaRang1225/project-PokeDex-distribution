8ee09bfe8d80a70cdddb56c5a896936579575eee
QuaRang <dbduddnd1225@gmail.com>
2024-05-17 14:54:12 +0900

    #8 - 사전 준비
    - 포켓몬 도감에 표시할 색상 Assets파일에 추가
    - Color 확장하여 사용 코드 추가
    - 테스트를 위한 커스텀 데이터 추가
    - 지역 및 타입 필터 열거 추가

---
+++ PokeDex_Project/Custom/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  PokeDex_Project
+//
+//  Created by 유영웅 on 5/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static func typeColor(_ types:String)->Color{
+        return Color(types)
+    }
+}
---
+++ PokeDex_Project/Filter/TypeFilter.swift
@@ -0,0 +1,30 @@
+//
+//  TypeFilter.swift
+//  PokeDex_Project
+//
+//  Created by 유영웅 on 5/17/24.
+//
+
+import Foundation
+
+enum TypeFilter: String,CaseIterable {
+    case bug = "벌레"
+    case normal = "노말"
+    case fairy = "페어리"
+    case dark = "악"
+    case flying = "비행"
+    case dragon = "드래곤"
+    case electric = "전기"
+    case fighting = "격투"
+    case fire = "불꽃"
+    case ghost = "고스트"
+    case grass = "풀"
+    case ground = "땅"
+    case ice = "얼음"
+    case poison = "독"
+    case psychic = "에스퍼"
+    case rock = "바위"
+    case steel = "강철"
+    case water = "물"
+    case unknown = "???"
+}
