6e04a3ab899d168d789ff3a1704a2e72b83acd9a
QuaRang <dbduddnd1225@gmail.com>
2024-05-25 14:47:15 +0900

    #포켓몬 결정력 내구력 화면 관련
    - 경정력/내구력을 선택하기 위한 여러 메서드 집합 Viewmodel 생성

--- PokeDex_Project/ViewModel/CalculateViewModel.swift
+++ PokeDex_Project/ViewModel/CalculateViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class CalculateViewModel:ObservableObject{
+    
+    @Published var calculate:CalculateFilter = .attack
+    
+    func indicatorOffset(width:CGFloat)->CGFloat{
+        let index = CGFloat(getIndex())
+        if index == 0{return 0}
+        let buttonWidth = width/CGFloat(CalculateFilter.allCases.count)
+        
+        return index * buttonWidth
+        
+    }
+    func getIndex() ->Int{
+        switch calculate{
+        case .attack:
+            return 0
+        case .defense:
+            return 1
+        }
+    }
+}
+
+
+
