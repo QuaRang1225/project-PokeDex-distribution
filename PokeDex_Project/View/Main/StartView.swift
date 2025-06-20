a335c1ac3aeff300f80181c57a17d8df83ea72cd
QuaRang1225 <dbduddnd1225@gmail.com>
2025-06-20 10:47:45 +0900

    feat: 서버 URL 수정

---
+++ PokeDex_Project/View/Main/StartView.swift
@@ -0,0 +1,20 @@
+//
+//  StartView.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 5/20/25.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Image("logo")
+            .resizable()
+            .frame(width: 100,height: 100)
+    }
+}
+
+#Preview {
+    StartView()
+}
