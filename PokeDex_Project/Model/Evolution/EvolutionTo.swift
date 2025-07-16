5025040465751bd7012f26f2afa3c978ff38bf1a
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-16 15:39:38 +0900

    refector: #17 - 주석처리

--- PokeDex_Project/Model/Evolution/EvolutionTo.swift
+++ PokeDex_Project/Model/Evolution/EvolutionTo.swift
@@ -7,24 +7,24 @@
 
 import Foundation
 
-struct EvolutionTo:Codable,Hashable,Equatable{
-    var id: Int?
-    var evolTo: [EvolutionTo]
-    var image: [String]
-    var name: String
+/// 진화트리 구조 요소
+struct EvolutionTo: Codable, Hashable, Equatable {
+    
+    var id: Int?    /// ID
+    var evolTo: [EvolutionTo]   /// 진화할  포켓몬
+    var image: [String]     /// 이미지
+    var name: String        // 이름
 
     enum CodingKeys: String, CodingKey {
         case id = "_id"
         case evolTo = "evol_to"
         case image, name
     }
+    
     init(image: [String], name: String, evolTo: [EvolutionTo] = []) {
         self.image = image
         self.name = name
         self.evolTo = evolTo
-    }
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.name == rhs.name
     }
 }
 
