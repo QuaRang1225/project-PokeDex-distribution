d5abf2d82185fc2f90f1688097db60c9fee85387
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-16 22:23:51 +0900

    refactor: #17 - 리전폼 클라이언트 구현
    - 다른 모습 요청
    - 의존성 키 정의 (요청 및 데이터 반환)
    - 의존성 값 정의 (외부 접근용)

--- PokeDex_Project/API/Service/VarietiesClient.swift
+++ PokeDex_Project/API/Service/VarietiesClient.swift
@@ -5,18 +5,35 @@
 //  Created by 유영웅 on 5/17/24.
 //
 
+import ComposableArchitecture
 import Foundation
-import Alamofire
-import Combine
 
-enum VarietiesApiService{
-    static func variety(name:String)-> AnyPublisher<VarietiesRespons,AFError>{
-        print("포켓몬 폼 api 호출")
-        return ApiClient.shared.session
-            .request(VarietiesRouter.variety(name:name))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: VarietiesRespons.self)
-            .value()
-            .eraseToAnyPublisher()
+/// 다른 모습 APIClient
+struct VarietiesClient {
+    typealias ReturnVarieties = @Sendable (_ name:String) async throws -> VarietiesRespons
+    
+    /// 다른 모습 요청
+    var fetchVariety: ReturnVarieties
+}
+
+// MARK: - 각 요청에 해당하는 의존성 키 주입
+extension VarietiesClient: DependencyKey {
+    static var liveValue: VarietiesClient {
+        let variety: ReturnVarieties = { name in
+            let request = VarietiesRouter.variety(name: name).makeURLRequest()
+            return try await URLSession.shared.requestDecoding(VarietiesRespons.self, urlRequest: request)
+        }
+        return VarietiesClient(fetchVariety: variety)
+    }
+}
+
+// MARK: - 외부에서 접근하기 위해 의존성 값 반환
+extension DependencyValues {
+    var varietiesClient: VarietiesClient {
+        get {
+            self[VarietiesClient.self]
+        } set {
+            self[VarietiesClient.self] = newValue
+        }
     }
 }
