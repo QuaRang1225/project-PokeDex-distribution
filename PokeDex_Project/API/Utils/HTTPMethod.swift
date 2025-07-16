62db1124245294c1e0575b14f5f5efb635943c5b
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-16 17:35:24 +0900

    refactor: #17 - API 관련 코드 관련
    - APIClient 및 Logger 삭제
    - Config, HTTPMethod, Router 인터페이스 추가

---
+++ PokeDex_Project/API/Utils/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/16/25.
+//
+
+import Foundation
+
+/// HTTP 메서드 디코딩
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
