f4fc13c30455c1ee0098d2a4e13868f25ef421be
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-16 22:21:58 +0900

    refactor: #17 - 포켓몬 클라이언트 구현
    - 포켓몬/포켓몬 리스트 요청
    - 의존성 키 정의 (요청 및 데이터 반환)
    - 의존성 값 정의 (외부 접근용)

--- PokeDex_Project/API/Service/PokemonClient.swift
+++ PokeDex_Project/API/Service/PokemonClient.swift
@@ -6,27 +6,47 @@
 //
 
 import Foundation
-import Alamofire
-import Combine
+import ComposableArchitecture
 
-enum PokemonAPIService {
-    static func pokemon(id:Int) -> AnyPublisher<PokemonResponse,AFError>{
-        print("포켓몬 api 호출")
-        return ApiClient.shared.session
-            .request(PokemonRouter.pokemon(id: id))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: PokemonResponse.self)
-            .value()
-            .eraseToAnyPublisher()
-    }
+/// 포켓몬 APIClient
+struct PokemonClient {
+    typealias ReturnPokemon = @Sendable (_ id: Int) async throws -> PokemonResponse
+    typealias ReturnPokemons = @Sendable (
+        _ page: Int,
+        _ region: String,
+        _ type_1: String,
+        _ type_2: String,
+        _ query: String
+    ) async throws -> PokemonListResponse
     
-    static func pokemons(page:Int,region:String,type_1:String,type_2:String,query:String)-> AnyPublisher<PokemonListResponse,AFError>{
-        print("포켓몬 api 호출")
-        return ApiClient.shared.session
-            .request(PokemonRouter.pokemons(page: page, region: region, type_1: type_1, type_2: type_2, query: query))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: PokemonListResponse.self)
-            .value()
-            .eraseToAnyPublisher()
+    /// 단일 포켓몬 요청
+    var fetchPokemon: ReturnPokemon
+    /// 포켓몬 리스트 요청
+    var fetchPokemons: ReturnPokemons
+}
+
+// MARK: - 각 요청에 해당하는 의존성 키 주입
+extension PokemonClient: DependencyKey {
+    static var liveValue: PokemonClient {
+        let pokemon: ReturnPokemon = { id in
+            let request = PokemonRouter.pokemon(id: id).makeURLRequest()
+            return try await URLSession.shared.requestDecoding(PokemonResponse.self, urlRequest: request)
+        }
+        let pokemons: ReturnPokemons = { page, region, type_1, type_2, query in
+            let request = PokemonRouter.pokemons(page: page, region: region, type_1: type_1, type_2: type_2, query: query).makeURLRequest()
+            return try await URLSession.shared.requestDecoding(PokemonListResponse.self, urlRequest: request)
+        }
+        return PokemonClient(fetchPokemon: pokemon, fetchPokemons: pokemons)
+    }
+}
+
+// MARK: - 외부에서 접근하기 위해 의존성 값 반환
+extension DependencyValues {
+    var pokemonClient: PokemonClient {
+        get {
+            self[PokemonClient.self]
+        } set {
+            self[PokemonClient.self] = newValue
+        }
     }
 }
