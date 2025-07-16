4bba86edc1b845be002eaf1aafbfd3db68052e57
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-16 22:24:04 +0900

    refactor: #17 - 진화트리 클라이언트 구현
    - 진화트리 요청
    - 의존성 키 정의 (요청 및 데이터 반환)
    - 의존성 값 정의 (외부 접근용)

--- PokeDex_Project/API/Service/EvolutionClient.swift
+++ PokeDex_Project/API/Service/EvolutionClient.swift
@@ -6,17 +6,34 @@
 //
 
 import Foundation
-import Alamofire
-import Combine
+import ComposableArchitecture
 
+/// 진화트리 APIClient
 struct EvolutionClient {
-    static func evolution(num:Int)-> AnyPublisher<EvolutionTreeResponse,AFError>{
-        print("포켓몬 진화트리 api 호출")
-        return ApiClient.shared.session
-            .request(EvolutionRouter.evolutuon(num:num))
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type: EvolutionTreeResponse.self)
-            .value()
-            .eraseToAnyPublisher()
+    typealias ReturnEvolution = @Sendable (_ num: Int) async throws -> EvolutionTreeResponse
+    
+    /// 진화트리 요청
+    var fetchEvolution: ReturnEvolution
+}
+
+// MARK: - 각 요청에 해당하는 의존성 키 주입
+extension EvolutionClient: DependencyKey {
+    static var liveValue: EvolutionClient {
+        let evolution: ReturnEvolution = { num in
+            let request = EvolutionRouter.evolutuon(num: num).makeURLRequest()
+            return try await URLSession.shared.requestDecoding(EvolutionTreeResponse.self, urlRequest: request)
+        }
+        return EvolutionClient(fetchEvolution: evolution)
+    }
+}
+
+// MARK: - 외부에서 접근하기 위해 의존성 값 반환
+extension DependencyValues {
+    var evolutionClient: EvolutionClient {
+        get {
+            self[EvolutionClient.self]
+        } set {
+            self[EvolutionClient.self] = newValue
+        }
     }
 }
