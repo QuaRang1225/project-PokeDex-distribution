6d7167f1bb7df0a8719b5707e37a3b2ac94e2225
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-20 00:23:01 +0900

    refactor: #17 - 타입라벨 리펙토링

--- PokeDex_Project/Components/TypesView.swift
+++ PokeDex_Project/Components/TypesView.swift
@@ -7,27 +7,28 @@
 
 import SwiftUI
 
+/// 타입 뷰
 struct TypesView: View {
     let type:String
     let width:CGFloat?
     let height:CGFloat
     let font:Font
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 5)
+        Capsule()
             .frame(height: height)
             .frame(maxWidth: width)
             .foregroundColor(Color.typeColor(type))
             .overlay {
                 Text(type)
                     .font(font)
-                    .padding(.horizontal,15)
+                    .padding(.horizontal, 15)
                     .foregroundColor(.white)
-                    .shadow(color:.black, radius: 1)
-                
+                    .shadow(color:.black, radius: 0.5)
             }
     }
 }
 
 #Preview {
-    TypesView(type: "불꽃", width: 150, height: 22, font: .callout)
+    TypesView(type: "불꽃", width: 150, height: 32, font: .body)
 }
