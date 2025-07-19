c09716c5ca055a7ec34ba299773a42fa7ce3e0fe
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-19 17:11:07 +0900

    refactor: #17 - 포켓몬 셀 정의

---
+++ PokeDex_Project/Features/Main/View/SubView/PokemonCell.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonCell.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/19/25.
+//
+
+import SwiftUI
+
+struct PokemonCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonCell()
+}
