45147863833b62514e0cb8bdf4cc7d4849754399
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-20 01:01:25 +0900

    refactor: #17 - 메인 Feature CellFeature와 연결

--- PokeDex_Project/Features/Main/Feature/MainFeature.swift
+++ PokeDex_Project/Features/Main/Feature/MainFeature.swift
@@ -12,6 +12,7 @@ import ComposableArchitecture
 struct MainFeature: Reducer {
     typealias PokemonResult = Result<PokemonList, NetworkError>
     
+    /// 포켓몬 쿼리 구조체
     struct PoekmonsQuery {
         var page: Int = 1
         var region: String = RegionFilter.national.rawValue
@@ -19,11 +20,14 @@ struct MainFeature: Reducer {
         var query: String = ""
     }
     
-    struct State: Equatable {
+    @ObservableState struct State: Equatable {
         var pokemons: PokemonList? = nil                                            // 포켓몬 리스트
         var isLoading: Bool = false                                                 // 로딩 중
         var showSearchBoard = false                                                 // 검색 보드뷰 표시
         var regionTitle: String = RegionFilter.national.rawValue                    // 지방 이름
+        
+        var selectedPokemon: Pokemon? = nil                                         // 선택한 포켓몬(상세 뷰를 위함)
+        var pokemonCellStates: IdentifiedArrayOf<PokemonCellFeature.State> = []     // 포켓몬 리스트 셀 상태 배열
         
         // 하위 Feature 상태 정의
         var searchBoardState = SearchBoardFeature.State()                           // 검색보드 상태
@@ -33,13 +37,18 @@ struct MainFeature: Reducer {
         case viewDidLoad                                                            // 뷰 로드 후
         case recievedPokemons(result: PokemonResult)                                // 데이터 요청 후
         case didTappedSearchButton                                                  // 검색 버튼 터치
-        case delegate(Delegate)
+        case delegate(Delegate)                                                     // 상위 Feature에서 구현할 액션
         
         // 하위 Feature 액션 정의
         case searchBoardAction(action: SearchBoardFeature.Action)                   // 검색보드 액션
         
+        // 포켓몬 선택 관련 액션 정의
+        case pokemonCellFeature(IdentifiedActionOf<PokemonCellFeature>)             // 포켓몬 셀 상태, 액션 정의
+        case didTappedPokemonCell(Pokemon)                                          // 상세화면 열기
+        case dismissPokemonDetail                                                   // 상세화면 닫기
+        
         enum Delegate: Equatable {
-            case selectedRegion(region: String)
+            case selectedRegion(region: String)                                     // 지역 선택
         }
     }
     
@@ -61,7 +70,16 @@ struct MainFeature: Reducer {
                 return executeSearchBoardFeature(&state, action: action)
             case let .delegate(.selectedRegion(region)):
                 return fetchPokemons(&state, query: PoekmonsQuery(region: region))
+            case let .didTappedPokemonCell(pokemon):
+                return movePokemonDetailsView(&state, pokemon: pokemon)
+            case .dismissPokemonDetail:
+                return dismissPokemonDetailsView(&state)
+            default:
+                return .none
             }
+        }
+        .forEach(\.pokemonCellStates, action: \.pokemonCellFeature) {
+            PokemonCellFeature()
         }
     }
     
@@ -87,6 +105,7 @@ struct MainFeature: Reducer {
         switch result {
         case let .success(pokemons):
             state.pokemons = pokemons
+            state.pokemonCellStates = IdentifiedArray(uniqueElements: pokemons.pokemons.map { PokemonCellFeature.State(pokemon: $0) })
         case let .failure(error):
             print(error.errorMessage)
         }
@@ -97,6 +116,18 @@ struct MainFeature: Reducer {
     /// 검색 보드 표시
     private func showSearchBoard(_ state: inout State) -> Effect<Action> {
         state.showSearchBoard.toggle()
+        return .none
+    }
+    
+    /// 포켓몬 상세 뷰 이동
+    private func movePokemonDetailsView(_ state: inout State, pokemon: Pokemon) -> Effect<Action> {
+        state.selectedPokemon = pokemon
+        return .none
+    }
+    
+    /// 포켓몬 상세 뷰 닫기
+    private func dismissPokemonDetailsView(_ state: inout State) -> Effect<Action> {
+        state.selectedPokemon = nil
         return .none
     }
 }
