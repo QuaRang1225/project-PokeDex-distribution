c07987b44cbb50d945266dad1e416389bbbe5b70
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-19 17:37:09 +0900

    refactor: #17 - 몬스터볼 이미지 확장을 위한 extension 추가

---
+++ PokeDex_Project/Features/Main/Feature/SubFeature/PokemonCellFeature.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonCellFeature.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/19/25.
+//
+
+import Foundation
---
+++ PokeDex_Project/Shared/Extension/String+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  String+Extension.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/19/25.
+//
+
+import Foundation
+
+extension String {
+    static let mosterBallImageURL: String = "https://github.com/PokeAPI/sprites/blob/master/sprites/items/poke-ball.png?raw=true"
+}
