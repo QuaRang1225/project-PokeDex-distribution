31aede2904d015cb78eca26aeb38d0130f992b4b
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-19 17:39:19 +0900

    refactor: #17 - 포켓몬 셀 Feature 구현
    - 이벤트는 상위에서 구현하도록 설정

--- PokeDex_Project/Features/Main/Feature/SubFeature/PokemonCellFeature.swift
+++ PokeDex_Project/Features/Main/Feature/SubFeature/PokemonCellFeature.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct PokemonCellFeature: Reducer {
+    
+    struct State: Equatable {
+        var state: Pokemon? = nil
+    }
+    
+    enum Action: Equatable {
+        case delegate(Delegate)
+        
+        enum Delegate: Equatable {
+            case didTapCell
+        }
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .delegate(.didTapCell):
+                return .none
+            }
+        }
+    }
+}
