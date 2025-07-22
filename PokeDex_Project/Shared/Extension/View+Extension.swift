fc3eceb63336dba0ed01fac8339fd190ccadeeae
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-22 12:51:37 +0900

    refactor: #17 - 파일 정의
    - ViewModifier 및 View 확장으로 추가

---
+++ PokeDex_Project/Shared/Extension/View+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  View+Extension.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/22/25.
+//
+
+import Foundation
+
+/// View 관련 확장사항
+extension View {
+    func onVisible(_ action: @escaping () -> Void) -> some View {
+        self.modifier(VisibleModifier(action: action))
+    }
+}
