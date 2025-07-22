62cc4d3bc072516ee69de2fa995f278f201bcf25
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-22 22:31:14 +0900

    refactor: #17 - 포켓몬 커스텀 데이터
    - 포켓몬 정보
    - 진화트리 정보

--- PokeDex_Project/Custom/CustomData.swift
+++ PokeDex_Project/Custom/CustomData.swift
@@ -7,15 +7,37 @@
 
 import Foundation
 
+/// 커스텀 데이터
 class CustomData{
     
     static let instance = CustomData()
     private init(){}
     
-    var pokemon = Pokemons(id: 271, color: "green", base: Base(image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/271.png", types: ["물","풀"]), captureRate: 120, dex: [Dex(num: 271, region: "전국"),Dex(num: 20, region: "호연(구)"),Dex(num: 56, region: "칼로스 마운틴")], eggGroup: ["수중 1","식물"], evolutionTree: 136, formsSwitchable: false, genderRate: 4, genra: "명랑함포켓몬", hatchCounter: 15, name: "로토스", textEntries: TextEntries(text: [ "햇볕이 잘 드는 물가에 산다.\n낮에는 수초로 된 침대에서\n자고 해가 지면 활동하기 시작한다.","낚시꾼을 발견하면\n물속에서 낚싯줄을 잡아당겨\n방해하며 좋아하는 장난꾸러기다.","해 질 무렵이 되면 활동하는 야행성이다.\n낚시꾼을 발견하면 물속에서 줄을\n당겨 방해하고선 기뻐한다.","몸 전체가 미끈미끈한 점액으로 덮여 있어서\n그 손에 닿으면 매우 기분 나쁘다.\n곧잘 인간의 아이로 오인된다.","해질녘이 되면 활동을\n시작하는 야행성 포켓몬이다.\n강바닥의 이끼를 먹는다.","햇볕이 잘 드는 물가에 산다.\n낮에는 수초로 된 침대에서 자고\n해가 지면 활동하기 시작한다."], version: [ "X","Y","오메가루비","알파사파이어","소드","실드"]), varieties: ["lombre"])
-    var pokemonList = [
-        PokemonsList(id: 1, color: "green", name: "이상해씨", base: ListBase(image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png", types: ["독","풀"], num: 1)),
-        PokemonsList(id: 2, color: "green", name: "이상해풀", base: ListBase(image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png", types: ["독","풀"], num: 2)),
-        PokemonsList(id: 3, color: "green", name: "이상해꽃", base: ListBase(image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png", types: ["독","풀"], num: 3))
-    ]
+    /// 포켓몬 데이터
+    var pokemon = Pokemon(id: 271, color: "green", base: Base(image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/271.png", types: ["물","풀"]), captureRate: 120, dex: [Dex(num: 271, region: "전국"),Dex(num: 20, region: "호연(구)"),Dex(num: 56, region: "칼로스 마운틴")], eggGroup: ["수중 1","식물"], evolutionTree: 136, formsSwitchable: false, genderRate: 4, genra: "명랑함포켓몬", hatchCounter: 15, name: "로토스", textEntries: TextEntries(text: [ "햇볕이 잘 드는 물가에 산다.\n낮에는 수초로 된 침대에서\n자고 해가 지면 활동하기 시작한다.","낚시꾼을 발견하면\n물속에서 낚싯줄을 잡아당겨\n방해하며 좋아하는 장난꾸러기다.","해 질 무렵이 되면 활동하는 야행성이다.\n낚시꾼을 발견하면 물속에서 줄을\n당겨 방해하고선 기뻐한다.","몸 전체가 미끈미끈한 점액으로 덮여 있어서\n그 손에 닿으면 매우 기분 나쁘다.\n곧잘 인간의 아이로 오인된다.","해질녘이 되면 활동을\n시작하는 야행성 포켓몬이다.\n강바닥의 이끼를 먹는다.","햇볕이 잘 드는 물가에 산다.\n낮에는 수초로 된 침대에서 자고\n해가 지면 활동하기 시작한다."], version: [ "X","Y","오메가루비","알파사파이어","소드","실드"]), varieties: ["lombre"])
+    
+    /// 포켓몬 진화트리
+    var evolutionTree = EvolutionTo(
+        image: [
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/56.png"
+        ],
+        name: "망키",
+        evolTo: [
+            EvolutionTo(
+                image:[
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/57.png"
+                ],
+                name: "성원숭",
+                evolTo: [
+                    EvolutionTo(
+                        image:[
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/979.png"
+                        ],
+                        name: "저승갓숭",
+                        evolTo: []
+                    )
+                ]
+            )
+        ]
+    )
 }
