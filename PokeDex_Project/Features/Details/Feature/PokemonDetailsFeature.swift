d9c47c5c43c68d8009de9dbc75244a99cd34d866
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-22 17:42:00 +0900

    refactor: #17 - 포켓몬 피쳐 및 광고 폴더링 변경
    - 포켓몬 상세보기 Feature 정의

---
+++ PokeDex_Project/Features/Details/Feature/PokemonDetailsFeature.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonDetailsFeature.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/22/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct PokemonDetailsFeature: Reducer {
+    
+    @ObservableState struct State: Equatable {
+        
+    }
+    
+    @CasePathable enum Action: Equatable {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            
+        }
+    }
+}
