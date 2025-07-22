533bf5807414953ab54e9c6066de5458980b9e8f
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-22 15:34:35 +0900

    refactor: #17 - 검색 바 구현
    - 기존 SwiftUI 버전으로 구현

---
+++ PokeDex_Project/Components/SearchBar.swift
@@ -0,0 +1,60 @@
+//
+//  SearchBar.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/22/25.
+//
+
+import Foundation
+import SwiftUI
+
+/// 검색바 구현
+struct SearchBar: View {
+    @State var text = ""
+    
+    var body: some View {
+        HStack {
+            imageView
+            texField
+            Spacer()
+            clearButton
+        }
+        .padding(8)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(.systemGray6))
+        )
+        .padding(.horizontal)
+    }
+}
+
+// MARK: - 검색바 컴포넌트
+extension SearchBar {
+    /// 돋보기 이미지
+    private var imageView: some View {
+        Image(systemName: "magnifyingglass")
+            .foregroundStyle(.gray)
+    }
+    /// 텍스트 필드
+    private var texField: some View {
+        TextField("포켓몬 이름", text: $text)
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+    }
+    /// 삭제 버튼
+    @ViewBuilder
+    private var clearButton: some View {
+        if !text.isEmpty {
+            Button {
+                text = ""
+            } label: {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundStyle(.gray)
+            }
+        }
+    }
+}
+
+#Preview {
+    SearchBar(text: "")
+}
