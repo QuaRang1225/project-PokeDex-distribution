00469796eaf3c0d31e84e319e7064417c2315050
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-26 05:28:24 +0900

    refactor: #17 - String 확장 구현

--- PokeDex_Project/Shared/Extension/String+Extension.swift
+++ PokeDex_Project/Shared/Extension/String+Extension.swift
@@ -8,10 +8,21 @@
 import Foundation
 import SwiftUI
 
+// MARK: - 문자열 관련 메서드 추가 구현
 extension String {
     static let mosterBallImageURL: String = "https://github.com/PokeAPI/sprites/blob/master/sprites/items/poke-ball.png?raw=true"
     
+    /// 문자열 -> 색상
     var typeColor: Color {
         return Color(self)
+    }
+
+    /// 문자열에서 숫자 추출
+    var extractNumber: Int? {
+        let pattern = "\\d+"
+        if let range = self.range(of: pattern, options: .regularExpression) {
+            return Int(self[range])
+        }
+        return nil
     }
 }
