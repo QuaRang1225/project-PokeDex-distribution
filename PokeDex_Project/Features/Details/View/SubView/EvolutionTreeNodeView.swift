df96e405bbef887dccba7ddabfebd405de2af683
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-28 22:39:15 +0900

    refactor: #17 - 진화트리 뷰 리펙토링
    - 진화트리뷰가 재귀해서 표시 되도록 구현

--- PokeDex_Project/Features/Details/View/SubView/EvolutionTreeNodeView.swift
+++ PokeDex_Project/Features/Details/View/SubView/EvolutionTreeNodeView.swift
@@ -7,56 +7,80 @@
 
 import SwiftUI
 import Kingfisher
+import ComposableArchitecture
 
-struct EvolTreeNodeView: View {
-    let items = Array(repeating: GridItem(.flexible()), count: 3)
-    let node: EvolutionTo?
+struct EvolutionTreeNodeView: View {
+    typealias EvolutionTreeStore = ViewStoreOf<EvolutionTreeFeature>
+    let store: StoreOf<EvolutionTreeFeature>
+    
     
     var body: some View {
-        VStack {
-            Button {
-                
-            } label: {
-                VStack {
-                    HStack {
-                        ForEach(node?.image ?? [], id: \.self){ image in
-                            KFImage(URL(string: image))
-                                .placeholder{
-                                    Color.gray.opacity(0.2)
-                                }
-                                .resizable()
-                                .frame(width: 70,height: 70)
-                                .cornerRadius(10)
-                            
-                        }
-                    }
-                    Text(node?.name ?? "").bold().font(.caption)
-                }
-                .foregroundColor(.primary)
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack {
+                imageButton(viewStore: viewStore)
+                chevrongImageView(viewStore: viewStore)
+                nextPokemon(viewStore: viewStore)
             }
-            if let evolTo = node?.evolTo, !evolTo.isEmpty {
-                Image(systemName: "chevron.down")
-                    .font(.title2)
-                    .padding(.vertical,10)
-            }
-            HStack (alignment: .top){
-                if let evolTo = node?.evolTo, evolTo.count > 3{
-                    LazyVGrid(columns: items){
-                        ForEach(evolTo,id: \.self) { child in
-                            EvolTreeNodeView(node: child)
-                                .padding()
-                        }
-                    }
-                }else{
-                    ForEach(node?.evolTo ?? [],id: \.self) { child in
-                        EvolTreeNodeView(node: child)
-                    }
-                }
+            .onDidLoad {
+                viewStore.send(.viewDidLoad)
             }
         }
     }
 }
 
+extension EvolutionTreeNodeView {
+    /// 이미지 버튼
+    private func imageButton(viewStore: EvolutionTreeStore) -> some View {
+        Button {
+            viewStore.send(.didTappedEvolutionTo)
+        } label: {
+            VStack {
+                HStack {
+                    ForEach(viewStore.state.node.image, id: \.self){ image in
+                        KFImage(URL(string: image))
+                            .placeholder{
+                                Color.gray.opacity(0.2)
+                            }
+                            .resizable()
+                            .frame(width: 70,height: 70)
+                            .cornerRadius(10)
+                    }
+                }
+                Text(viewStore.state.node.name).bold().font(.caption)
+            }
+            .foregroundColor(.primary)
+        }
+    }
+    /// 화살표 버튼
+    @ViewBuilder
+    private func chevrongImageView(viewStore: EvolutionTreeStore) -> some View {
+        if !viewStore.state.node.evolTo.isEmpty {
+            Image(systemName: "chevron.down")
+                .font(.title2)
+                .padding(.vertical,10)
+        }
+    }
+    /// 진화트리 재귀 뷰
+    @ViewBuilder
+    private func nextPokemon(viewStore: EvolutionTreeStore) -> some View {
+        let columns = Array(repeating: GridItem(.flexible()), count: 3)
+        HStack(alignment: .top) {
+            ForEachStore(
+                store.scope(state: \.children, action: \.children)
+            ) { childStore in
+                EvolutionTreeNodeView(store: childStore)
+            }
+            .lazyVGrid(columns: columns, condition: viewStore.node.evolTo.count > 3)
+        }
+    }
+}
+
 #Preview {
-    EvolTreeNodeView(node: CustomData.instance.evolutionTree)
+    let store = Store(
+        initialState: EvolutionTreeFeature.State(
+            node: CustomData.instance.evolutionTree
+        )) {
+        EvolutionTreeFeature()
+        }
+    EvolutionTreeNodeView(store: store)
 }
