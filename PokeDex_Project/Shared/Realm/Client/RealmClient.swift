d2e41547b4cc421cab868536c1aaacacfdf8588b
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-28 22:54:20 +0900

    refactor: #17 - RealmClient 수정
    - 각 메서드 마다 Realm() 객체를 가지도록 구현
    - 필터링 첨자 수정
    - 에러처리 수정

--- PokeDex_Project/Shared/Realm/Client/RealmClient.swift
+++ PokeDex_Project/Shared/Realm/Client/RealmClient.swift
@@ -14,8 +14,9 @@ struct RealmClient {
     typealias StorePokemon = @Sendable (_ pokemon: RealmObject) async throws -> Void
     typealias FetchPokemon = @Sendable (_ num: Int) async throws -> RealmPokemon
     typealias FetchPokemons = @Sendable () async throws -> [RealmPokemon]
-    typealias DeletePokemon = @Sendable (_ pokemon: RealmObject) async throws -> Void
+    typealias DeletePokemon = @Sendable (_ num: Int) async throws -> Void
     typealias DeleteAllPokemons = @Sendable () async throws -> Void
+    typealias ReadIsBookmarked = @Sendable (_ num: Int) async throws -> Bool
     
     /// 컨텍스트 저장
     var storePokemon: StorePokemon
@@ -27,17 +28,29 @@ struct RealmClient {
     var deletePokemon: DeletePokemon
     /// 포켓몬 전체삭제
     var deleteAllPokemons: DeleteAllPokemons
+    /// 북마크 여부 조회
+    var readIsBookmarked: ReadIsBookmarked
 }
 
 extension RealmClient: DependencyKey {
-    static func liveValue(realm: Realm = try! Realm()) -> RealmClient {
+    static var liveValue: RealmClient {
+        return liveValues()
+    }
+    
+    static func liveValues() -> RealmClient {
         // 컨텍스트 저장
         let storePokemon: StorePokemon = { pokemon in
-            try! realm.write { realm.add(pokemon) }
+            do {
+                let realm = try Realm()
+                try realm.write { realm.add(pokemon) }
+            } catch {
+                throw RealmError.storeFailed
+            }
         }
         // 포켓몬 요청
         let fetchPokemon: FetchPokemon = { num in
-            guard let pokemonData = realm.objects(RealmObject.self).filter("num == \(num)").first else { throw RealmError.notFound }
+            let realm = try Realm()
+            guard let pokemonData = realm.objects(RealmObject.self).filter("num == %@", num).first else { throw RealmError.notFound }
             return RealmPokemon(
                 id: pokemonData.id.stringValue,
                 num: pokemonData.num,
@@ -48,6 +61,7 @@ extension RealmClient: DependencyKey {
         }
         // 포켓몬 리스트 요청
         let fetchPokemons: FetchPokemons = {
+            let realm = try Realm()
             var pokemons:[RealmPokemon] = []
             realm.objects(RealmObject.self).forEach { data in
                 pokemons.append(
@@ -64,17 +78,33 @@ extension RealmClient: DependencyKey {
         }
         // 포켓몬 삭제
         let deletePokemon: DeletePokemon = { num in
+            let realm = try Realm()
             guard let pokemon = realm
                 .objects(RealmObject.self)
-                .filter("num == \(num)")
-                .first else { throw RealmError.deleteFailed }
-            try! realm.write {
-                realm.delete(pokemon)
+                .filter("num == %@", num)
+                .first else { throw RealmError.notFound }
+            do {
+                try realm.write {
+                    realm.delete(pokemon)
+                }
+            } catch {
+                throw RealmError.deleteFailed
             }
         }
         // 포켓몬 모두 삭제
         let deleteAll: DeleteAllPokemons = {
-            try! realm.write { realm.deleteAll() }
+            do {
+                let realm = try Realm()
+                try realm.write { realm.deleteAll() }
+            } catch {
+                throw RealmError.deleteFailed
+            }
+        }
+        // 북마크 여부 조회
+        let readIsBookmarked: ReadIsBookmarked = { num in
+            let realm = try Realm()
+            let exists = realm.objects(RealmObject.self).filter("num == %@", num).isEmpty == false
+            return exists
         }
         // MARK: - 반환
         return RealmClient(
@@ -82,10 +112,10 @@ extension RealmClient: DependencyKey {
             fetchPokemon: fetchPokemon,
             fetchPokemons: fetchPokemons,
             deletePokemon: deletePokemon,
-            deleteAllPokemons: deleteAll
+            deleteAllPokemons: deleteAll,
+            readIsBookmarked: readIsBookmarked
         )
     }
-    static var liveValue: RealmClient { liveValue() }
 }
 
 extension DependencyValues {
