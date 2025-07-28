7910764fa74dedbcd466900ce16c24baa8d73ed7
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-28 22:36:06 +0900

    refactor: #17 - 실버디 및 아르세우스 필터 추가
    - 실버디와 아르세우스는 폼체인지로 타입이 바뀌기 때문에 해당 로직을 TypeFilter에 추가

--- PokeDex_Project/Filter/TypeFilter.swift
+++ PokeDex_Project/Filter/TypeFilter.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum TypeFilter: String,CaseIterable {
+enum TypeFilter: String, CaseIterable {
     case bug = "벌레"
     case normal = "노말"
     case fairy = "페어리"
@@ -26,5 +26,42 @@ enum TypeFilter: String,CaseIterable {
     case rock = "바위"
     case steel = "강철"
     case water = "물"
-    case unknown = "???"
+//    case unknown = "???"
+}
+
+// MARK: - 실버디
+extension TypeFilter {
+    init?(pokemonNumber: Int, rawValue: String) {
+        switch pokemonNumber {
+        case 493:
+            if let type = TypeFilter(rawValue: rawValue) {
+                self = type
+            } else {
+                return nil
+            }
+        case 773:
+            switch rawValue {
+            case "타입:노말": self = .normal
+            case "타입:파이팅": self = .fighting
+            case "타입:플라잉": self = .flying
+            case "타입:포이즌": self = .poison
+            case "타입:그라운드": self = .ground
+            case "타입:락": self = .rock
+            case "타입:버그": self = .bug
+            case "타입:고스트": self = .ghost
+            case "타입:스틸": self = .steel
+            case "타입:파이어": self = .fire
+            case "타입:워터": self = .water
+            case "타입:그래스": self = .grass
+            case "타입:일렉트릭": self = .electric
+            case "타입:사이킥": self = .psychic
+            case "타입:아이스": self = .ice
+            case "타입:드래곤": self = .dragon
+            case "타입:다크": self = .dark
+            case "타입:페어리": self = .fairy
+            default: return nil
+            }
+        default: return nil
+        }
+    }
 }
