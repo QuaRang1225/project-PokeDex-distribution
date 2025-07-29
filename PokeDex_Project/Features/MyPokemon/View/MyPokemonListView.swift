9afbedba1cc4739957cb61f10cedf44904627c27
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-29 16:38:19 +0900

    refactor: #17 - 포켓몬 북마크 리스트 뷰 리펙토링
    - 기존 뷰모델을 사용하는 로직에서 Feature 사용 뷰로 수정
    - 컴포넌트 extension으로 그룹화

--- PokeDex_Project/Features/MyPokemon/View/MyPokemonListView.swift
+++ PokeDex_Project/Features/MyPokemon/View/MyPokemonListView.swift
@@ -11,60 +11,95 @@ import ComposableArchitecture
 
 /// 북마크 리스트 뷰
 struct MyPokemonListView: View {
-    typealias BookmarkStore = ViewStoreOf<MyPokemonListFeature>
+    typealias MyPokemonListStore = ViewStoreOf<MyPokemonListFeature>
     let store: StoreOf<MyPokemonListFeature>
     
     var body: some View {
-        VStack{
-            HStack{
-                Text("내 포켓몬")
-                    .bold()
-                    .font(.title)
-                Spacer()
-                Button {
-                    RealmManager.deleteAll()
-                    pokemonList = RealmManager.fetchPokemons()
-                } label: {
-                    HStack{
-                        Text("전체삭제").font(.caption)
-                        Image(systemName: "trash")
-                    }
-                    
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            VStack(spacing: 0) {
+                HStack {
+                    titleLabel
+                    Spacer()
+                    removeAllButton(viewStore: viewStore)
                 }
-                .foregroundColor(.primary)
-            }.padding(.horizontal)
-            
-            List{
-                ForEach(pokemonList,id: \.self){ pokemon in
-                    NavigationLink {
-                        PokemonView(pokemonId: pokemon.num, hasAppeared: .constant(false))
-                            .navigationBarBackButtonHidden()
-                    } label: {
-                        DexNumView(pokemon: pokemon)
-                    }
-
-                    
+                .padding([.horizontal, .bottom])
+                Divider()
+                if viewStore.state.isLoading {
+                    Spacer()
+                    ProgressView()
+                        .scaleEffect(2.0)
+                    Spacer()
+                } else {
+                    bookmarkListView(viewStore: viewStore)
                 }
-                .onMove(perform:moveList)
-                .onDelete(perform:removeList)
-                
-            }.listStyle(.inset)
-                .font(.callout)
-            
-        }
-        .onAppear{
-            pokemonList = RealmManager.fetchPokemons()
+            }
+            .onAppear {
+                viewStore.send(.viewDidLoad)
+            }
+            .navigationDestination(
+                item: viewStore.binding(
+                    get: \.pokemonDetailState,
+                    send: { _ in .dismissPokemonDetail } // nil로 바꿀 때 액션 전달
+                )
+            ) { id in
+                pokemonDetailsView
+            }
         }
     }
-    func removeList(at offsets: IndexSet) {
-        offsets.forEach { RealmManager.deletePokemon(num:pokemonList[$0].num)}
-        
-   }
-    func moveList(indices: IndexSet, newOffset: Int) {
-        pokemonList.move(fromOffsets: indices, toOffset: newOffset)
-       }
 }
 
+// MARK: - 북마크 리스트 컴포넌트 정의
+private extension MyPokemonListView {
+    /// 타이틀 라벨
+    var titleLabel: some View {
+        Text("내 포켓몬")
+            .bold()
+            .font(.title)
+    }
+    /// 전체 삭제 버튼
+    func removeAllButton(viewStore: MyPokemonListStore) -> some View {
+        Button {
+            viewStore.send(.removeAllPokemon)
+        } label: {
+            Label {
+                Text("전체삭제")
+                    .font(.caption)
+            } icon: {
+                Image(systemName: "trash")
+            }
+        }
+        .foregroundColor(.primary)
+    }
+    /// 리스트 뷰
+    func bookmarkListView(viewStore: MyPokemonListStore) -> some View {
+        List{
+            ForEach(viewStore.pokemons,id: \.self){ pokemon in
+                Button {
+                    viewStore.send(.didTappedPokemonCell(pokemon.num))
+                } label: {
+                    DexNumView(pokemon: pokemon)
+                }
+            }
+            .onMove { fromOffset, toOffset in
+                viewStore.send(.movePokemon(fromOffset, toOffset))
+            }
+            .onDelete { offsets in
+                viewStore.send(.removePokemon(offsets))
+            }
+        }
+        .listStyle(.inset)
+        .font(.callout)
+    }
+    /// 포켓몬 상세 뷰
+    private var pokemonDetailsView: some View {
+        IfLetStore(
+            store.scope(state: \.pokemonDetailState, action: \.pokemonDetailsAction)
+        ) { store in
+            PokemonDetailsView(store: store)
+                .navigationBarBackButtonHidden(true)
+        }
+    }
+}
 #Preview {
     let store = Store(initialState: MyPokemonListFeature.State()) { MyPokemonListFeature() }
     MyPokemonListView(store: store)
