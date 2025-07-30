f6bdb77aba99175bc3209591a5f178ccf5d56b3f
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-30 20:38:20 +0900

    refactor: #17 - 공격/방어 결정력 계산 Feature 구현

---
+++ PokeDex_Project/Features/Calculator/Feature/SubFeature/DefenseFeature.swift
@@ -0,0 +1,8 @@
+//
+//  DefenseFeature.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/30/25.
+//
+
+import Foundation
---
+++ PokeDex_Project/Features/Calculator/Feature/SubFeature/PowerFeature.swift
@@ -0,0 +1,8 @@
+//
+//  PowerFeature.swift
+//  PokeDex_Project
+//
+//  Created by Quarang on 7/30/25.
+//
+
+import Foundation
