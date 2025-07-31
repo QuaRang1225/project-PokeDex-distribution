1d8789cba8f2e1b3a4a6d0c6b08228fcdc16fb43
QuaRang1225 <dbduddnd1225@gmail.com>
2025-07-31 22:21:45 +0900

    refactor: #17 - 디펜스 Feature 폴더 이동

--- PokeDex_Project/Features/Calculator/Feature/SubFeature/DefenseFeature.swift
+++ PokeDex_Project/Features/Calculator/Feature/SubFeature/DefenseFeature.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct DefenseFeature: Reducer {
+    
+    @ObservableState struct State: Equatable {
+        var value: DefenseValue
+    }
+    
+    @CasePathable enum Action: Equatable {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}
