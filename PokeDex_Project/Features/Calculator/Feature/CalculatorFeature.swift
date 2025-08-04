bf7155b0d6f3e35c6ee0053f70aea8e8ed3773ca
QuaRang1225 <dbduddnd1225@gmail.com>
2025-08-04 16:34:27 +0900

    refactor: #17 - 포켓몬 계산 Feature 구현
    - 상태를 넘겨 받아서 결정력과 내구력을 옵션에 따라 구현할 수 있도록 액션과 상태 정의

--- PokeDex_Project/Features/Calculator/Feature/CalculatorFeature.swift
+++ PokeDex_Project/Features/Calculator/Feature/CalculatorFeature.swift
@@ -10,20 +10,71 @@ import ComposableArchitecture
 
 /// 계산기 Feature
 struct CalculatorFeature: Reducer {
+    /// 공격/방어 모드
+    enum Mode {
+        case power
+        case defense
+    }
     
     @ObservableState struct State: Equatable {
+        var pokemonInfo: PokemonInfo
+        var mode: Mode = .power
         
+        var powerState: PowerFeature.State? = nil
+        var defenseState: DefenseFeature.State? = nil
     }
     
     @CasePathable enum Action: Equatable {
+        case viewDidLoad
+        case selectedMode(_ mode: Mode)         /// 모드 선택 시
+        case delegate(Delegate)
         
+        case powerAction(_ action: PowerFeature.Action)
+        case defenseAction(_ action: DefenseFeature.Action)
+        
+        enum Delegate {
+            case didTappedDismissButton             /// 닫기 버튼 선택 시
+        }
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-                
+            case .viewDidLoad:
+                return initializeValue(&state)
+            case let .selectedMode(mode):
+                return changedMode(&state, mode: mode)
+            default:
+                return .none
             }
         }
+        .ifLet(\.powerState, action: \.powerAction) {
+            PowerFeature()
+        }
+        .ifLet( \.defenseState, action: \.defenseAction) {
+            DefenseFeature()
+        }
+    }
+    private func initializeValue(_ state: inout State) -> Effect<Action> {
+        state.powerState = PowerFeature.State(
+            value: PokemonValue(
+                type: state.pokemonInfo.types,
+                pysical: state.pokemonInfo.stats[1],
+                special: state.pokemonInfo.stats[2]
+            )
+        )
+        state.defenseState = DefenseFeature.State(
+            value: PokemonValue(
+                type: state.pokemonInfo.types,
+                pysical: state.pokemonInfo.stats[2],
+                special: state.pokemonInfo.stats[4]
+            )
+        )
+        return .none
+    }
+    /// 모드 변경
+    private func changedMode(_ state: inout State, mode: Mode) -> Effect<Action> {
+        state.mode = mode
+        return .none
     }
 }
