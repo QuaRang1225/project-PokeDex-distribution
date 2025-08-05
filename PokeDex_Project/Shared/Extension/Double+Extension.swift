f70dabfad0a3e9caf299fecd36de1eb32397f4ee
QuaRang1225 <dbduddnd1225@gmail.com>
2025-08-05 17:21:09 +0900

    refactor: #17 - 랭크업 계산 확장

--- PokeDex_Project/Shared/Extension/Double+Extension.swift
+++ PokeDex_Project/Shared/Extension/Double+Extension.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension Double {
+    /// 랭크업 계산
+    var calculateRankMultiplier: Double {
+        switch self {
+        case 1...6:
+            return Double(2 + self) / 2.0
+        case -6 ... -1:
+            return 2.0 / Double(2 - self) // 음수 처리 주의
+        default:
+            return 1.0 // 0랭크일 경우 보정 없음
+        }
+    }
+}
