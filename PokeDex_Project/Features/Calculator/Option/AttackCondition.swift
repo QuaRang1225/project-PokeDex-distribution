56f81e4835ae0c8441d1328c356ab9ca0be54de0
QuaRang1225 <dbduddnd1225@gmail.com>
2025-08-06 17:15:58 +0900

    refactor: #17 - 공격 종류 조건 열거형 변경
    - pokemon state를 모두 열거형으로 파라미터를 수정했기 때문에 그에 따라 수정

--- PokeDex_Project/Features/Calculator/Option/AttackCondition.swift
+++ PokeDex_Project/Features/Calculator/Option/AttackCondition.swift
@@ -8,11 +8,10 @@
 import Foundation
 
 /// 포켓몬 기술의 공격 종류를 나타내는 열거형
-enum AttackCategory: String, CaseIterable {
+enum AttackCondition: String, CaseIterable {
     
     /// 물리 공격
     case physical = "물리공격"
-    
     /// 특수 공격
     case special = "특수공격"
     
