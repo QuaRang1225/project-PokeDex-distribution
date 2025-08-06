db7c1c30b160621ba128f1244a3c633bd884370d
QuaRang1225 <dbduddnd1225@gmail.com>
2025-08-06 17:16:49 +0900

    refactor: #17 - 날씨 조건 열거형 변경
    - pokemon state를 모두 열거형으로 파라미터를 수정했기 때문에 그에 따라 수정

--- PokeDex_Project/Features/Calculator/Option/WeatherCondition.swift
+++ PokeDex_Project/Features/Calculator/Option/WeatherCondition.swift
@@ -8,20 +8,21 @@
 import Foundation
 
 /// 게임 필드의 날씨 상태를 나타내는 열거형
-enum WeatherCondition: String, CaseIterable {
+enum WeatherCondition: String, CaseIterable, LosslessStringConvertible {
     
     case none = "없음"
     /// 모래바람이 부는 상태
     case sandstorm = "모래바람"
-    
     /// 눈이 내리는 상태 (설경)
     case snow = "설경"
-    
     /// 쾌청하여 햇살이 강한 상태
     case sunny = "쾌청"
-    
     /// 비가 내리는 상태
     case rain = "비바라기"
+    
+    var description: String {
+        self.rawValue
+    }
     
     /// 한글 날씨 이름을 반환합니다.
     var koreanName: String {
@@ -32,18 +33,18 @@ enum WeatherCondition: String, CaseIterable {
     func calculate(state: inout PokemonState) -> PokemonState {
         switch self {
         case .sunny:
-            if TypeFilter(rawValue: state.type) == .fire {
+            if state.type == .fire {
                 state.result *= 1.5
-            } else if TypeFilter(rawValue: state.type) == .water {
+            } else if state.type == .water {
                 state.result /= 2.0
             }
             // 불꽅 타입 위력 1.5배 증가
             // 물 타입 위력 0.5배로 감소
             return state
         case .rain:
-            if TypeFilter(rawValue: state.type) == .water {
+            if state.type == .water {
                 state.result *= 1.5
-            } else if TypeFilter(rawValue: state.type) == .fire {
+            } else if state.type == .fire {
                 state.result /= 2.0
             }
             // 물타입 위력 1.5배 증가
@@ -53,12 +54,8 @@ enum WeatherCondition: String, CaseIterable {
         }
     }
     
-    /// 원시값 -> case
-    init?(rawValue: String) {
-        if let match = WeatherCondition.allCases.first(where: { $0.rawValue == rawValue }) {
-            self = match
-        } else {
-            return nil
-        }
+    // 문자열 → 타입으로 변환
+    init?(_ description: String) {
+        self.init(rawValue: description)
     }
 }
